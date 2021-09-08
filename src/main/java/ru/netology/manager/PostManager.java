b910a10f8a3f6f01b68d7ad4728ca1dc05095798
Oleg Atlasov <olegkui@mail.ru>
2021-09-08 15:01:41 +0400

    Added PostManager.java

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,19 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    private int ownerId;
+    private int postId;
+
+
+    public Post[] search() {
+        // add some logic
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+        // add some logic
+    }
+}
